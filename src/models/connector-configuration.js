19c2886382c7c223da5439d7aeaa874839272ac3
Rafael Iop <rafael@send4.com.br>
2019-06-24 11:07:19 -0300

    auth_fields renamed to custom_fields

--- src/models/connector-configuration.js
+++ src/models/connector-configuration.js
@@ -4,7 +4,7 @@
  * @param {string} props.name
  * @param {string} props.description
  * @param {string} props.image_url
- * @param {object[]} props.auth_fields
+ * @param {object[]} props.custom_fields
  * @param {string[]} props.default_statuses
  * @param {string[]} props.default_wiretransfer_methods
  * @param {string} props.pay_refund_method
@@ -14,14 +14,14 @@ function ConnectorConfiguration(props) {
   if (typeof props.name === 'string'
   && typeof props.description === 'string'
   && typeof props.image_url === 'string'
-  && typeof props.auth_fields === 'object'
+  && typeof props.custom_fields === 'object'
   && typeof props.default_statuses === 'object'
   && typeof props.default_wiretransfer_methods === 'object'
   && typeof props.pay_refund_method === 'string') {
     this.name = props.name;
     this.description = props.description;
     this.image_url = props.image_url;
-    this.auth_fields = props.auth_fields;
+    this.custom_fields = props.custom_fields;
     this.default_statuses = props.default_statuses;
     this.default_wiretransfer_methods = props.default_wiretransfer_methods;
     this.pay_refund_method = props.pay_refund_method;
